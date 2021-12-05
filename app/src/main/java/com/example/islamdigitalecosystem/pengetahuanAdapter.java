977cbc22eabbab9f44bf39a11830178d1703c432
Fikri Abdullah <fikriabdullahed@gmail.com>
2021-12-05 21:24:54 +0700

    midtrans update successfull -ish

--- app/src/main/java/com/example/islamdigitalecosystem/pengetahuanAdapter.java
+++ app/src/main/java/com/example/islamdigitalecosystem/pengetahuanAdapter.java
@@ -19,6 +19,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -38,12 +39,15 @@ import com.midtrans.sdk.corekit.models.snap.TransactionResult;
 import com.midtrans.sdk.uikit.SdkUIFlowBuilder;
 
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Random;
 
 public class pengetahuanAdapter extends RecyclerView.Adapter<pengetahuanAdapter.MyViewHolder> {
     ArrayList<pengetahuanData> pengetahuanData;
     Context context;
     private static final String TAG = "pengetahuanAdapter";
-    private DatabaseReference firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private FirebaseDatabase firebaseDatabase;
     FirebaseAuth firebaseAuth;
     FirebaseFirestore db;
     int hargaMateri;
@@ -65,7 +69,7 @@ public class pengetahuanAdapter extends RecyclerView.Adapter<pengetahuanAdapter.
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
        final String judulMateri = pengetahuanData.get(position).getMateriName();
        holder.setBabList(judulMateri);
-       firebaseDatabase = FirebaseDatabase.getInstance().getReference();
+       firebaseDatabase = FirebaseDatabase.getInstance();
        firebaseAuth = FirebaseAuth.getInstance();
        db = FirebaseFirestore.getInstance();
 
@@ -73,7 +77,6 @@ public class pengetahuanAdapter extends RecyclerView.Adapter<pengetahuanAdapter.
        holder.goMateri.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(final View view) {
-
                //retrieve harga materi dan apakah materi berbayar
                db.collection("Materi").document(judulMateri).collection(judulMateri)
                        .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
@@ -93,73 +96,152 @@ public class pengetahuanAdapter extends RecyclerView.Adapter<pengetahuanAdapter.
                    }
                });
 
-               firebaseDatabase.child("UserDatabase").child("Student").child(firebaseAuth.getCurrentUser().getUid())
-                       .child("MateriOwned");
+               //retrive is materi owned
+               databaseReference = firebaseDatabase.getReference().child("UserDatabase")
+                       .child("Student").child(Objects.requireNonNull(firebaseAuth.getCurrentUser()).getUid());
+               databaseReference.addValueEventListener(new ValueEventListener() {
+                   @Override
+                   public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                       dataSnapshot.getChildren();
+                       boolean isOwned = dataSnapshot.child("MateriOwned").child(judulMateri).exists();
+                       Log.d(TAG, "materi Owned : " + isOwned);
+                       Log.d(TAG, "judul materi " + judulMateri);
 
-                   firebaseDatabase.addValueEventListener(new ValueEventListener() {
-                       @Override
-                       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                           dataSnapshot.getChildren();
-                           String isOwned = dataSnapshot.child(judulMateri).getValue(String.class);
-                           Log.d(TAG, "materi Owned : " + isOwned);
 
-                           if ((isOwned == null && isPaid)){
-                               if (ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
-                                   ActivityCompat.requestPermissions((Activity)view.getContext(), new String[]{Manifest.permission.READ_PHONE_STATE}, 101);
-                               }
-                               Log.d(TAG, "materi is paid, and not owned");
-                               SdkUIFlowBuilder.init()
-                                       .setClientKey("SB-Mid-client-PewuNr7kEHDpz8cu")
-                                       .setContext(view.getContext())
-                                       .setTransactionFinishedCallback(new TransactionFinishedCallback() {
-                                           @Override
-                                           public void onTransactionFinished(TransactionResult transactionResult) {
-                                               Log.d(TAG, "transaction Finsished!! " + transactionResult.getStatus());
+                       if ((!isOwned) && (!isPaid)){//materi berbayar, belum lunas
+                           //bayar ke midtrans
+                           if (ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                               ActivityCompat.requestPermissions((Activity)view.getContext(), new String[]{Manifest.permission.READ_PHONE_STATE}, 101);
+                           }
+                           Log.d(TAG, "materi is paid, and not owned");
+                           SdkUIFlowBuilder.init()
+                                   .setClientKey("SB-Mid-client-PewuNr7kEHDpz8cu")
+                                   .setContext(view.getContext())
+                                   .setTransactionFinishedCallback(new TransactionFinishedCallback() {
+                                       @Override
+                                       public void onTransactionFinished(TransactionResult transactionResult) {
+                                           Log.d(TAG, "transaction Finsished!! " + transactionResult.getStatus());
+                                           if (transactionResult.getResponse() != null) {
+                                               switch (transactionResult.getStatus()) {
+                                                   case TransactionResult.STATUS_SUCCESS:
+                                                       Toast.makeText(view.getContext(), "Transaction Finished. ID: " + transactionResult.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+                                                       Log.d(TAG, "transaction Status : " + transactionResult.getResponse().getTransactionId());
 
+                                                       databaseReference.child("UserDatabase").child("Student").child(firebaseAuth.getCurrentUser().getUid())
+                                                               .child("MateriOwned").child(judulMateri).setValue(judulMateri)
+                                                               .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                   @Override
+                                                                   public void onSuccess(Void unused) {
+                                                                       Log.d(TAG, "transaction saved!!");
+                                                                       Intent intent = new Intent(view.getContext(), materiBab1.class);
+                                                                       intent.putExtra("babMateriRef", judulMateri);
+                                                                       context.startActivity(intent);
+                                                                   }
+                                                               }).addOnFailureListener(new OnFailureListener() {
+                                                           @Override
+                                                           public void onFailure(@NonNull Exception e) {
+                                                               Log.d(TAG, "sasving transaction failed "+e.getMessage());
+                                                               Toast.makeText(view.getContext(), "Saving To Database Failed", Toast.LENGTH_LONG).show();
+                                                           }
+                                                       });
+                                                       break;
+                                                   case TransactionResult.STATUS_PENDING:
+                                                       //Toast.makeText(view.getContext(), "Transaction Pending. ID: " + transactionResult.getResponse().getTransactionId(), Toast.LENGTH_LONG).show();
+                                                       Log.d(TAG, "transaction Status : " + transactionResult.getResponse().getTransactionId());
+
+                                                       databaseReference.child("UserDatabase").child("Student").child(firebaseAuth.getCurrentUser().getUid())
+                                                               .child("MateriOwned").child(judulMateri).setValue(judulMateri)
+                                                               .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                                   @Override
+                                                                   public void onSuccess(Void unused) {
+                                                                       Log.d(TAG, "transaction saved!!");
+                                                                       Intent intent = new Intent(view.getContext(), materiBab1.class);
+                                                                       intent.putExtra("babMateriRef", judulMateri);
+                                                                       context.startActivity(intent);
+                                                                   }
+                                                               }).addOnFailureListener(new OnFailureListener() {
+                                                           @Override
+                                                           public void onFailure(@NonNull Exception e) {
+                                                               Log.d(TAG, "sasving transaction failed "+e.getMessage());
+                                                               Toast.makeText(view.getContext(), "Saving To Database Failed", Toast.LENGTH_LONG).show();
+                                                           }
+                                                       });
+                                                       break;
+
+                                                   case TransactionResult.STATUS_FAILED:
+                                                       Toast.makeText(view.getContext(), "Transaction Failed. ID: " + transactionResult.getResponse().getTransactionId() + ". Message: " + transactionResult.getResponse().getStatusMessage(), Toast.LENGTH_LONG).show();
+                                                       Log.d(TAG, "transaction Status : " + transactionResult.getResponse().getTransactionId());
+                                                       break;
+                                               }
+                                               transactionResult.getResponse().getValidationMessages();
+                                           } else if (transactionResult.isTransactionCanceled()) {
+                                               Toast.makeText(view.getContext(), "Transaction Canceled", Toast.LENGTH_LONG).show();
+                                               Log.d(TAG, "Transaction Canceled");
+                                           } else {
+                                               if (transactionResult.getStatus().equalsIgnoreCase(TransactionResult.STATUS_INVALID)) {
+                                                   Toast.makeText(view.getContext(), "Transaction Invalid", Toast.LENGTH_LONG).show();
+                                                   Log.d(TAG, "Transaction Invalid");
+                                               } else {
+                                                   Toast.makeText(view.getContext(), "Transaction Finished with failure.", Toast.LENGTH_LONG).show();
+                                                   Log.d(TAG, "Transaction Finished with failure.");
+                                               }
                                            }
-                                       }).setMerchantBaseUrl("https://ihya-elearning.herokuapp.com/")
-                                       .enableLog(true)
-                                       .setLanguage("id")
-                                       .buildSDK();
 
-                               FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                               TransactionRequest transactionRequest = new TransactionRequest(String.valueOf(Math.random()), hargaMateri);
+                                       }
+                                   }).setMerchantBaseUrl("https://ihya-elearning.herokuapp.com/")
+                                   .enableLog(true)
+                                   .setLanguage("id")
+                                   .buildSDK();
 
-                               com.midtrans.sdk.corekit.models.ItemDetails itemDetails = new ItemDetails("Materi Bahasa Arab", hargaMateri, 1 , judulMateri);
-                               ArrayList<ItemDetails> itemDetailsArrayList = new ArrayList<>();
-                               itemDetailsArrayList.add(itemDetails);
-                               transactionRequest.setItemDetails(itemDetailsArrayList);
+                           Random random = new Random();
+                           long orderID = random.nextLong();
+                           FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                           TransactionRequest transactionRequest = new TransactionRequest(String.valueOf(orderID), hargaMateri);
 
-                               CustomerDetails customerDetails = new CustomerDetails();
-                               String userIdentifier = firebaseAuth.getCurrentUser().getDisplayName();
-                               customerDetails.setCustomerIdentifier(userIdentifier);
-                               customerDetails.getBillingAddress();
-                               customerDetails.getShippingAddress();
-                               customerDetails.getPhone();
-                               customerDetails.getEmail();
+                           com.midtrans.sdk.corekit.models.ItemDetails itemDetails = new ItemDetails("Materi Bahasa Arab", hargaMateri, 1 , judulMateri);
+                           ArrayList<ItemDetails> itemDetailsArrayList = new ArrayList<>();
+                           itemDetailsArrayList.add(itemDetails);
+                           transactionRequest.setItemDetails(itemDetailsArrayList);
 
-                               transactionRequest.setCustomerDetails(customerDetails);
+                           CustomerDetails customerDetails = new CustomerDetails();
+                           String userIdentifier = firebaseAuth.getCurrentUser().getDisplayName();
+                           customerDetails.setCustomerIdentifier(userIdentifier);
+                           customerDetails.getBillingAddress();
+                           customerDetails.getShippingAddress();
+                           customerDetails.getPhone();
+                           customerDetails.getEmail();
 
-                               MidtransSDK.getInstance().setTransactionRequest(transactionRequest);
-                               MidtransSDK.getInstance().startPaymentUiFlow(view.getContext());
-                           }else if (isOwned == null || !isPaid){ //materi is not owned or materi is free
-                               Log.d(TAG, "materi is free or owned");
-                               Intent intent = new Intent(context, materiBab1.class);
-                               intent.putExtra("babMateriRef", judulMateri);
-                               context.startActivity(intent);
-                           }else{
-                               try {
-                                   throw new Exception("Something Went Wrong");
-                               } catch (Exception e) {
-                                   Log.d(TAG, "Clicking Materi : " + e.getMessage());
-                               }
+                           transactionRequest.setCustomerDetails(customerDetails);
+
+                           MidtransSDK.getInstance().setTransactionRequest(transactionRequest);
+                           MidtransSDK.getInstance().startPaymentUiFlow(view.getContext());
+
+                       }else if ((isOwned) || (isPaid)){ //materi is has ben lunas
+                           Log.d(TAG, "materi is lunas");
+                           Intent intent = new Intent(context, materiBab1.class);
+                           intent.putExtra("babMateriRef", judulMateri);
+                           context.startActivity(intent);
+                       }else if ((!isOwned) && (isPaid)){ //materi tidak berbayar
+                           Log.d(TAG, "materi is free");
+                           Intent intent = new Intent(context, materiBab1.class);
+                           intent.putExtra("babMateriRef", judulMateri);
+                           context.startActivity(intent);
+                       }
+                       else{
+                           try {
+                               throw new Exception("Something Went Wrong");
+                           } catch (Exception e) {
+                               Toast.makeText(view.getContext(), "Something Went Wrong", Toast.LENGTH_LONG).show();
+                               Log.d(TAG, "Clicking Materi : " + e.getMessage());
                            }
                        }
-                       @Override
-                       public void onCancelled(@NonNull DatabaseError databaseError) {
-                           Log.d(TAG, "Database Error : " + databaseError.getMessage());
-                       }
-                   });
+                   }
+
+                   @Override
+                   public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                   }
+               });
            }
        });
         holder.materiImage.setImageResource(R.drawable.aljabar);
